c31fdc6bc8b743e8c39e60d6dcec01cb2a23cce0
tgs266 <siegel.tucker@gmail.com>
2021-09-27 14:49:59 -0400

    initial

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+func GetConfig(configName string) {
+	viper.SetConfigName("local")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config/")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln("Env local not found")
+		os.Exit(1)
+	}
+
+	viper.SetConfigName(configName)
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config/")
+	viper.AutomaticEnv()
+
+	err = viper.MergeInConfig()
+	if err != nil {
+		log.Fatalln("Env " + configName + " not found")
+		os.Exit(1)
+	}
+}
