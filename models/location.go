22eb863b3a71f888a50b50acb7cb34bdca365a0f
tgs266 <siegel.tucker@gmail.com>
2021-09-27 17:58:26 -0400

    add locations to common

---
+++ models/location.go
@@ -0,0 +1,14 @@
+package models
+
+type Location struct {
+	Longitude float64 `bson:"longitude"`
+	Latitude  float64 `bson:"latitude"`
+}
+
+type Address struct {
+	AddressLine1 string `bson:"address_line_1"`
+	AddressLine2 string `bson:"address_line_2"`
+	Zipcode      string `bson:"zipcode"`
+	City         string `bson:"city"`
+	State        string `bson:"state"`
+}
