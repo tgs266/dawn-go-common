af65dc77ec1a2dcfba2247c66023c8db285ee468
Tucker Gary Siegel <tgsiegel@terpmail.umd.edu>
2021-10-30 14:22:02 -0400

    minor update

--- testing/request.go
+++ testing/request.go
@@ -9,11 +9,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func TestGetRequest(app *fiber.App, endpoint string, params *url.Values, token string, response interface{}) {
+func TestGetRequest(app *fiber.App, endpoint string, params *url.Values, token string, response interface{}) int {
 	req, _ := http.NewRequest("GET", "http://test.com"+endpoint, strings.NewReader(params.Encode()))
 	req.Header.Set("Authorization", "token")
 
 	httpResponse, _ := app.Test(req)
 	defer httpResponse.Body.Close()
 	json.NewDecoder(httpResponse.Body).Decode(&response)
+	return httpResponse.StatusCode
 }
