90489b6edef29acc2251056612e7132a6f866d61
Tucker Gary Siegel <tgsiegel@terpmail.umd.edu>
2021-10-30 14:18:40 -0400

    add new testing feature

---
+++ testing/request.go
@@ -0,0 +1,19 @@
+package testing
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestGetRequest(app fiber.App, endpoint string, params *url.Values, token *string, response interface{}) {
+	req, _ := http.NewRequest("GET", "http://test.com"+endpoint, strings.NewReader(params.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	httpResponse, _ := app.Test(req)
+	defer httpResponse.Body.Close()
+	json.NewDecoder(httpResponse.Body).Decode(&response)
+}
