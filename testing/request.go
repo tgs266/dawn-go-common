2cb6456fa503393d76830fb5ce9494c250caac7c
Tucker Gary Siegel <tgsiegel@terpmail.umd.edu>
2021-10-30 14:43:09 -0400

    update

--- testing/request.go
+++ testing/request.go
@@ -28,3 +28,14 @@ func TestGetRequestParams(app *fiber.App, endpoint string, params url.Values, to
 	json.NewDecoder(httpResponse.Body).Decode(&response)
 	return httpResponse.StatusCode
 }
+
+func TestPostRequest(app *fiber.App, endpoint string, params url.Values, token string, response interface{}) int {
+	req, _ := http.NewRequest("POST", "http://test.com"+endpoint, strings.NewReader(params.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "token")
+
+	httpResponse, _ := app.Test(req)
+	defer httpResponse.Body.Close()
+	json.NewDecoder(httpResponse.Body).Decode(&response)
+	return httpResponse.StatusCode
+}
