b427370bcadd5fc1fbaad7b511b97fe2bf7466ec
Tucker Siegel <tgsiegel@umd.edu>
2021-11-14 16:27:21 -0500

    test

--- messaging/connection.go
+++ messaging/connection.go
@@ -2,6 +2,7 @@ package messaging
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/streadway/amqp"
 )
@@ -12,12 +13,15 @@ var (
 )
 
 func Connect() error {
+	fmt.Println("trying to connect")
 	if Conn != nil || !Alive {
 		conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 		Conn = conn
 		if err != nil {
+			fmt.Println("cant connect")
 			Alive = false
 		} else {
+			fmt.Println("connected")
 			Alive = true
 		}
 		return err
