19cc14bc9a7376a890eb30fdf17ab050edea3d8e
Tucker Siegel <tgsiegel@umd.edu>
2021-11-14 16:53:37 -0500

    fix

--- messaging/connection.go
+++ messaging/connection.go
@@ -12,16 +12,16 @@ var (
 
 func Connect() error {
 	fmt.Println("trying to connect")
-	if Conn != nil {
+	if Conn == nil {
 		conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 		Conn = conn
 		if err != nil {
 			fmt.Println("connection failed")
 			fmt.Println(err)
+			return nil
 		} else {
 			fmt.Println("success")
 		}
-		return err
 	}
 	return nil
 }
