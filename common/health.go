dec8b3ba573153cf6f9a983cddc0b3b0f399f2fd
Tucker Siegel <tgsiegel@umd.edu>
2021-11-16 18:44:41 -0500

    send dead

--- common/health.go
+++ common/health.go
@@ -134,18 +134,20 @@ func StartHeartbeatMessenger() {
 }
 
 func SendDeadHeartbeat() {
-	hostname, _ := os.Hostname()
+	if viper.GetBool("app.messaging") {
+		hostname, _ := os.Hostname()
 
-	healthStruct := GetHealthStruct()
-	if healthStruct.Status != LastHeartbeat.Status {
-		heartBeat := messaging.Heartbeat{
-			Status:      "unavailable",
-			DBStatus:    healthStruct.DBStatus,
-			HostName:    hostname,
-			RequireAuth: viper.GetBool("security.auth"),
-			ContextPath: viper.GetString("server.context-path"),
+		healthStruct := GetHealthStruct()
+		if healthStruct.Status != LastHeartbeat.Status {
+			heartBeat := messaging.Heartbeat{
+				Status:      "unavailable",
+				DBStatus:    healthStruct.DBStatus,
+				HostName:    hostname,
+				RequireAuth: viper.GetBool("security.auth"),
+				ContextPath: viper.GetString("server.context-path"),
+			}
+			messaging.PublishHeartbeat(heartBeat)
+			LastHeartbeat = heartBeat
 		}
-		messaging.PublishHeartbeat(heartBeat)
-		LastHeartbeat = heartBeat
 	}
 }
