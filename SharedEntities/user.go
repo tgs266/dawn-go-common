7e8dfd40b5ee80462a14d4b6bf9d04ae77d9e9f4
Tucker Siegel <tgsiegel@umd.edu>
2021-12-04 19:19:20 -0500

    add shared entities

---
+++ SharedEntities/user.go
@@ -0,0 +1,31 @@
+package sharedentities
+
+import "time"
+
+type Address struct {
+	AddressLine1 string `bson:"address_line_1"`
+	AddressLine2 string `bson:"address_line_2"`
+	City         string `bson:"city"`
+	State        string `bson:"state"`
+	Zipcode      string `bson:"zipcode"`
+}
+type Location struct {
+	ID          string  `bson:"id"`
+	Latitude    float64 `bson:"latitude"`
+	Longitude   float64 `bson:"longitude"`
+	Address     string  `bson:"address"`
+	DisplayName string  `bson:"display_name"`
+}
+
+type User struct {
+	ID    string `bson:"_id"`
+	Name  string `bson:"name"`
+	Email string `bson:"email"`
+	Salt  string `bson:"salt"`
+	Hash  []byte `bson:"hash"`
+
+	LastLoggedIn time.Time `bson:"last_logged_in"`
+
+	DefaultLocation   Location   `bson:"default_location"`
+	FavoriteLocations []Location `bson:"favorite_locations"`
+}
