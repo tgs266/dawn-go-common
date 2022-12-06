f34a67161341d306db29875dbd3bf9e70d78a2f9
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-12-06 02:21:47 -0500

    spelling

--- validation/shared.go
+++ validation/shared.go
@@ -7,7 +7,7 @@ import (
 type SharedValFunc = func(value interface{}) error
 
 // some helpers for ozzo-validation
-func CreateSaredValidation(rules ...val.Rule) SharedValFunc {
+func CreateSharedValidation(rules ...val.Rule) SharedValFunc {
 	return func(value interface{}) error {
 		return val.Validate(value, rules...)
 	}
