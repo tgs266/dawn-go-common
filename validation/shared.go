d0ea15db7e1fbc1805cd9c090130f78f47ffaa09
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-12-06 02:20:34 -0500

    type

--- validation/shared.go
+++ validation/shared.go
@@ -4,8 +4,10 @@ import (
 	val "github.com/go-ozzo/ozzo-validation"
 )
 
+type SharedValFunc = func(value interface{}) error
+
 // some helpers for ozzo-validation
-func CreateSaredValidation(rules ...val.Rule) func(value interface{}) error {
+func CreateSaredValidation(rules ...val.Rule) SharedValFunc {
 	return func(value interface{}) error {
 		return val.Validate(value, rules...)
 	}
