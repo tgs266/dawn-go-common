1789621a76b023019ae7f951c8780d58ee297a8a
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2023-02-25 15:37:40 -0500

    add json tags

--- entities/user.go
+++ entities/user.go
@@ -3,18 +3,18 @@ package entities
 import "time"
 
 type Address struct {
-	AddressLine1 string `bson:"address_line_1"`
-	AddressLine2 string `bson:"address_line_2"`
-	City         string `bson:"city"`
-	State        string `bson:"state"`
-	Zipcode      string `bson:"zipcode"`
+	AddressLine1 string `bson:"address_line_1" json:"address_line_1"`
+	AddressLine2 string `bson:"address_line_2" json:"address_line_2"`
+	City         string `bson:"city" json:"city"`
+	State        string `bson:"state" json:"state"`
+	Zipcode      string `bson:"zipcode" json:"zipcode"`
 }
 type Location struct {
-	ID          string  `bson:"id"`
-	Latitude    float64 `bson:"latitude"`
-	Longitude   float64 `bson:"longitude"`
-	Address     string  `bson:"address"`
-	DisplayName string  `bson:"display_name"`
+	ID          string  `bson:"id" json:"id"`
+	Latitude    float64 `bson:"latitude" json:"latitude"`
+	Longitude   float64 `bson:"longitude" json:"longitude"`
+	Address     string  `bson:"address" json:"address"`
+	DisplayName string  `bson:"display_name" json:"display_name"`
 }
 
 var ROLE_INTS = []int{0, 1, 2}
@@ -30,20 +30,20 @@ var ROLES_REV = map[int]string{
 }
 
 type User struct {
-	ID    string `bson:"_id"`
-	Name  string `bson:"name"`
-	Email string `bson:"email"`
-	Salt  string `bson:"salt"`
-	Hash  []byte `bson:"hash"`
-	HashVersion string `bson:"hashVersion"`
-	Admin bool   `bson:"admin"`
-	Role  int    `bson:"role"`
-	NewsletterOptIn bool `bson:"newsletter_opt_in"`
+	ID              string `bson:"_id" json:"_id"`
+	Name            string `bson:"name" json:"name"`
+	Email           string `bson:"email" json:"email"`
+	Salt            string `bson:"salt" json:"salt"`
+	Hash            []byte `bson:"hash" json:"-"`
+	HashVersion     string `bson:"hashVersion" json:"-"`
+	Admin           bool   `bson:"admin" json:"-"`
+	Role            int    `bson:"role" json:"role"`
+	NewsletterOptIn bool   `bson:"newsletter_opt_in" json:"newsletter_opt_in"`
 
-	LastLoggedIn     time.Time `bson:"last_logged_in"`
-	LastTokenRefresh time.Time `bson:"last_token_refresh"`
-	CreatedAt        time.Time `bson:"created_at"`
+	LastLoggedIn     time.Time `bson:"last_logged_in" json:"last_logged_in"`
+	LastTokenRefresh time.Time `bson:"last_token_refresh" json:"-"`
+	CreatedAt        time.Time `bson:"created_at" json:"created_at"`
 
-	DefaultLocation   Location   `bson:"default_location"`
-	FavoriteLocations []Location `bson:"favorite_locations"`
+	DefaultLocation   Location   `bson:"default_location" json:"default_location"`
+	FavoriteLocations []Location `bson:"favorite_locations" json:"favorite_locations"`
 }
